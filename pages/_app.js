d9a2ce03413bd1c2af94094a3af23f709ab3337f
Cristian Berrios <cristian@ilogica.cl>
2022-06-18 01:55:58 -0400

    fix analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import "../styles/globals.scss";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+          ga('create', 'G-8EJ684QQ9B', 'auto');
+          ga('send', 'pageview');
+        `}
+      </Script>
+      <Script
+        src="https://www.google-analytics.com/analytics.js"
+        strategy="afterInteractive"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
