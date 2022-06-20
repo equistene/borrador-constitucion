f6719fa97cc4ee3e190a1698ea4a0b7e34e25c6f
Cristian Berrios <cristian@ilogica.cl>
2022-06-20 19:13:13 -0400

    add analytics plugin

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,11 @@
 import "../styles/globals.scss";
-import Script from "next/script";
+import GoogleAnalytics from "@bradgarropy/next-google-analytics";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <GoogleAnalytics measurementId="G-8EJ684QQ9B" />
       <Component {...pageProps} />
-
-      <Script
-        src="https://www.google-analytics.com/analytics.js?id=G-8EJ684QQ9B"
-        strategy="afterInteractive"
-      />
-
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-8EJ684QQ9B');
-        `}
-      </Script>
     </>
   );
 }
